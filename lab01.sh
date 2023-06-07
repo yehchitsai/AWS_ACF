eab991a9fcd59cbb1e3d632778112a3ab0e658dd
Chi-Tsai Yeh <yehchitsai@gmail.com>
2023-06-07 15:41:56 +0800

    Update lab01.sh

--- lab01.sh
+++ lab01.sh
@@ -3,6 +3,6 @@
 sudo dnf install python3-pip -y
 sudo pip install flask
 # Download Lab files
-wget https://flaskpythoncode.s3.amazonaws.com/main.py
+wget https://raw.githubusercontent.com/yehchitsai/AWS_ACF/main/main.py
 # Turn on web server
 nohup sudo python3 main.py &

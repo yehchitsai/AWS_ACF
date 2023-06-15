68f7eff26a81254951237be272e782eb85296f2d
Chi-Tsai Yeh <yehchitsai@gmail.com>
2023-06-15 13:48:24 +0800

    Update lab05.sh
    install crond service to start up flask server every reboot

--- lab05.sh
+++ lab05.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Install pip
-dnf install python3-pip -y
+# Install pip and crond
+dnf install python3-pip cronie -y
+systemctl start crond
 # Install Flask
 pip install flask
 # Download Lab files
-wget https://github.com/yehchitsai/AWS_ACF/raw/main/lab05.zip
+wget https://github.com/yehchitsai/AWS_ACF/raw/main/lab05.zip -O /home/ec2-user/lab05.zip
+cd /home/ec2-user
 unzip lab05.zip
 cd lab05
 # Turn on web server using Python Flask
+(crontab -l ; echo "@reboot cd /home/ec2-user/lab05 ; nohup sudo python3 main.py &") | crontab
 nohup sudo python3 main.py &
